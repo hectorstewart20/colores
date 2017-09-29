4d7f094a6c9bbc7070ab0fd64c5ce423513381ed
Hector Gonzalez <gonzalez.hector.83@gmail.com>
2017-09-29 01:37:49 -0300

    probando otro commit

--- src/Main.java
+++ src/Main.java
@@ -7,32 +7,6 @@ class Main {
 
     public static void main (String [ ] args) {
 
-        /*int i=0;
-        System.out.println("Primer inicial:");
-        Scanner dato1 = new Scanner(System.in);
-        int r = dato1.nextInt();
-        int g = dato1.nextInt();
-        int b = dato1.nextInt();
-        Color c = new Color(r,g,b);
-
-        while (i < 2){
-            i++;
-
-
-            System.out.println("sumar color:");
-            Scanner dato = new Scanner(System.in);
-
-            int r2 = dato.nextInt();
-            int g2 = dato.nextInt();
-            int b2 = dato.nextInt();
-
-            String[] nuevo_c = c.Suma(r2, g2, b2);
-
-
-            //String salida = Integer.toBinaryString(nuevo_c[0]);
-            System.out.println(Arrays.toString(nuevo_c));*/
-
-
         int i=0;
         System.out.println("Color inicial:");
         Scanner dato1 = new Scanner(System.in);
@@ -56,7 +30,7 @@ class Main {
             short b2 = dato1.nextShort();
             short[] nuevo_c = c.Suma(r2, g2, b2);
 
-            //String salida = Integer.toBinaryString(nuevo_c[0]);
+
             System.out.println(Arrays.toString(nuevo_c));
 
 
